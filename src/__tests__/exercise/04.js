07a84d7b097f9829298892f4549f50641fc8b92f
victor.longon <vhlongon@gmail.com>
2021-10-02 22:23:57 +0200

    solve exercise 4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,27 +4,36 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import faker from 'faker'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use userEvent.type to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+const buildLoginForm = ({
+  password = faker.internet.password(),
+  username = faker.internet.password(),
+} = {}) => ({
+  password,
+  username,
 })
+test('submitting the form calls onSubmit with username and password', () => {
+  const handleSubmit = jest.fn()
+  const {username, password} = buildLoginForm({
+    username: 'Bruce lee',
+    password: 'IIIIAAAH',
+  })
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+  render(<Login onSubmit={handleSubmit} />)
+
+  const userNameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
+  const submit = screen.getByRole('button', {name: /submit/i})
+
+  userEvent.type(userNameInput, username)
+  userEvent.type(passwordInput, password)
+
+  userEvent.click(submit)
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    username,
+    password,
+  })
+})
